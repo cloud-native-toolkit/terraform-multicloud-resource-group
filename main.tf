eecf3187540afd1788e938c7ecf1830cae0d5082
Sean Sundberg <seansund@us.ibm.com>
2022-02-20 08:42:51 -0600

    Uses count to disable module instead of enabled flag
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 locals {
-  group = var.cloud_provider == "ibm" ? module.ibm.group : (var.cloud_provider == "azure" ? module.azure.group : {})
+  group = var.cloud_provider == "ibm" ? module.ibm[0].group : (var.cloud_provider == "azure" ? module.azure[0].group : {})
 }
 
 module "ibm" {
   source = "github.com/cloud-native-toolkit/terraform-ibm-resource-group"
+  count = var.cloud_provider == "ibm" ? 1 : 0
 
-  enabled             = var.cloud_provider == "ibm"
   resource_group_name = var.resource_group_name
   provision           = var.provision
   sync                = var.sync
@@ -13,8 +13,8 @@ module "ibm" {
 
 module "azure" {
   source = "github.com/cloud-native-toolkit/terraform-azure-resource-group"
+  count = var.cloud_provider == "azure" ? 1 : 0
 
-  enabled             = var.cloud_provider == "azure"
   resource_group_name = var.resource_group_name
   provision           = var.provision
   region              = var.region
